1c853b5d91c6d6c2707a1e1cf5bd1ce7f006f7b6
Matthew Reimer <matt.reimer@gmail.com>
2017-01-10 15:59:32 -0800

    some basic stuff

--- centerlinexs/metrics.py
+++ centerlinexs/metrics.py
@@ -1,5 +1,35 @@
 from logger import Logger
+from raster import Raster
+import numpy as np
 
 def calcMetrics(validXS, rivershape, sDEM ):
+    """
+    Jhu Li: Do the thing!!!!!!
+    :param validXS:
+    :param rivershape:
+    :param sDEM:
+    :return:
+    """
     log = Logger('Metrics')
+
+    dem = Raster(sDEM)
+
+    for lineXS in validXS:
+        for xs in lineXS:
+            arr = interpolateRasterAlongLine(xs, dem)
+
     log.info("DOING MANY IMPORTANT THINGS!!!!!!")
+
+
+def interpolateRasterAlongLine(xs, raster):
+    """
+    Proof of concept. Get the raster values at every point along a cross section
+    :param xs:
+    :param raster:
+    :return:
+    """
+    points = [xs.interpolate(currDist) for currDist in np.arange(0, xs.length, 0.5)]
+    vals = []
+    for pt in points:
+        vals.append(raster.getPixelVal(pt.coords[0]))
+    return vals
