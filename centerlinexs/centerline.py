1d78a3e565fe146972fdc1edfac5cb1e985d4823
Matthew Reimer <matt.reimer@gmail.com>
2017-01-10 11:44:00 -0800

    better errors. more refactoring goodness

--- centerlinexs/centerline.py
+++ centerlinexs/centerline.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python
-import ogr
-import json
 import os, sys
 from shapely.geometry import *
 import argparse
@@ -10,13 +8,9 @@ from logger import Logger
 from vor import NARVoronoi
 from shapes import *
 from geosmoothing import *
+from plotting import Plotter
 
 # These are just for graphing
-import matplotlib.pyplot as plt
-from matplotlib.collections import PatchCollection
-from descartes import PolygonPatch
-
-
 def centerline(args):
 
     log = Logger("Centerline")
@@ -24,6 +18,7 @@ def centerline(args):
     # --------------------------------------------------------
     # Load the Shapefiles we need
     # --------------------------------------------------------
+    log.info("Opening Shapefiles...")
     rivershp = Shapefile(args.river.name)
     thalwegshp = Shapefile(args.thalweg.name)
     islandsshp = Shapefile(args.islands.name)
@@ -36,20 +31,19 @@ def centerline(args):
     islList = islandsshp.featuresToShapely()
     multipolIslands = MultiPolygon([isl['geometry'] for isl in islList if isl['fields']['Qualifying'] == 1])
 
-    # --------------------------------------------------------
-    # Make a new rivershape using the exterior and only
-    # qualifying islands from that shapefile
-    # --------------------------------------------------------
-
+    # Make a new rivershape using the exterior and only qualifying islands from that shapefile
+    log.info("Combining exterior and qualifying islands...")
     rivershape = Polygon(polyRiverShape.exterior).difference(multipolIslands)
-    riverspliner = GeoSmoothing()
+
     # The Spline smooth gives us round curves.
+    log.info("Spline Smoothing...")
+    riverspliner = GeoSmoothing()
     smoothRiver = riverspliner.smooth(rivershape)
     # The simplifyer reduces point count to make things manageable
     smoothRiver = smoothRiver.simplify(0.01)
 
     # --------------------------------------------------------
-    # Find the centerline
+    # Find the Centerline
     # --------------------------------------------------------
 
     # First and last line segment we need to extend
@@ -89,12 +83,14 @@ def centerline(args):
             points.append(RiverPoint(pt, interior=True, side=side, island=idx))
 
     # Here's where the Voronoi polygons come into play
+    log.info("Calculating Voronoi Polygons...")
     myVorL = NARVoronoi(points)
 
     # (OPTIONAL). Makes the polygons we will use to visualize
     myVorL.createshapes()
 
     # This is the function that does the actual work of creating the centerline
+    log.info("Spline Smoothing Main Line...")
     linespliner = GeoSmoothing(spl_smpar=5)
     centerline = myVorL.collectCenterLines()
     centerlineSmooth = linespliner.smooth(centerline)
@@ -104,6 +100,7 @@ def centerline(args):
     alternateLines = []
     for idx, island in enumerate(smoothRiver.interiors):
         altLine = myVorL.collectCenterLines(flipIsland=idx)
+        log.info("  Spline Smoothing Alternate line...")
         if altLine.type == "LineString":
             # We difference the alternate lines with the main line
             # to get just the bit that is different
@@ -114,6 +111,7 @@ def centerline(args):
     # Write the output Shapefile
     # --------------------------------------------------------
     # TODO: I'd love to abstract all this away but it's a pain to do this in a generic way
+    log.info("Writing Shapefiles...")
     outShape = Shapefile()
     outShape.create(args.centerline, rivershp.spatialRef, geoType=ogr.wkbMultiLineString)
 
@@ -140,69 +138,32 @@ def centerline(args):
     # Do a little show and tell with plotting and whatnot
     # --------------------------------------------------------
     if not args.noviz:
-        fig = plt.figure(1, figsize=(10, 10))
-        ax = fig.gca()
+        log.info("Plotting Results...")
+        plt = Plotter()
 
         # Left and right banks are light red and blue
-        plotShape(ax, bankshapes[0], '#DDCCCC', 1, 0)
-        plotShape(ax, bankshapes[1], '#AAAABB', 1, 0)
+        plt.plotShape(bankshapes[0], '#DDCCCC', 1, 0)
+        plt.plotShape(bankshapes[1], '#AAAABB', 1, 0)
 
         # The Voronoi shapes are light grey
-        plotShape(ax, myVorL.polys, '#444444', 0.1, 6)
+        # plt.plotShape(myVorL.polys, '#444444', 0.1, 6)
 
         # The rivershape is slightly green
-        plotShape(ax, rivershape, '#AACCAA', 0.4, 8)
-        plotShape(ax, smoothRiver, '#AAAACC', 0.2, 8)
+        plt.plotShape(rivershape, '#AACCAA', 0.4, 8)
+        plt.plotShape(smoothRiver, '#AAAACC', 0.2, 8)
 
         # Thalweg is green and where it extends to the bounding rectangle is orange
-        plotShape(ax, newThalweg, '#FFA500', 1, 15)
-        plotShape(ax, lineThalweg, '#00FF00', 1, 20)
+        plt.plotShape(newThalweg, '#FFA500', 1, 15)
+        plt.plotShape(lineThalweg, '#00FF00', 1, 20)
 
         # The centerline we choose is bright red
-        plotShape(ax, centerline, '#660000', 0.6, 30)
-        plotShape(ax, centerlineSmooth, '#FF0000', 0.8, 30)
+        plt.plotShape(centerline, '#660000', 0.6, 30)
+        plt.plotShape(centerlineSmooth, '#FF0000', 0.8, 30)
         # The alternate lines are in yellow
-        plotShape(ax, MultiLineString(alternateLines), '#FFFF00', 0.8, 25)
+        plt.plotShape(MultiLineString(alternateLines), '#FFFF00', 0.8, 25)
 
-        plt.ylim(rivershapeBounds.bounds[1], rivershapeBounds.bounds[3])
-        plt.xlim(rivershapeBounds.bounds[0], rivershapeBounds.bounds[2])
-        plt.autoscale(enable=False)
-        plt.show()
+        plt.showPlot(rivershapeBounds.bounds)
 
-
-def plotShape(ax, mp, color, alpha, zord):
-    """
-    Nothing fancy here. Just a plotting function to help us visualize
-    :param ax:
-    :param mp:
-    :param color:
-    :param alpha:
-    :param zord:
-    :return:
-    """
-    # We're using Descartes here for polygonpathc
-    if mp.type == 'Polygon':
-        ax.add_patch(PolygonPatch(mp, fc=color, ec='#000000', lw=0.2, alpha=alpha, zorder=zord))
-
-    elif mp.type == 'LineString':
-        x, y = mp.xy
-        ax.plot(x, y, color=color, alpha=alpha, linewidth=1, solid_capstyle='round', zorder=zord)
-
-    elif mp.type == 'MultiPoint':
-        for idx, p in enumerate(mp):
-            x, y = p.xy
-            ax.plot(x, y, color=color, alpha=alpha, markersize=2, marker=".", zorder=zord)
-
-    elif mp.type == 'MultiLineString':
-        for idx, p in enumerate(mp):
-            x, y = p.xy
-            ax.plot(x, y, color=color, alpha=alpha, linewidth=1, solid_capstyle='round', zorder=zord)
-
-    elif mp.type == 'MultiPolygon':
-        patches = []
-        for idx, p in enumerate(mp):
-            patches.append(PolygonPatch(p, fc=color, ec='#000000', lw=0.2, alpha=alpha, zorder=zord))
-        ax.add_collection(PatchCollection(patches, match_original=True))
 
 
 if __name__ == "__main__":
@@ -223,7 +184,7 @@ if __name__ == "__main__":
     parser.add_argument('centerline',
                         help='Path to the desired output centerline shapefile')
     parser.add_argument('--noviz',
-                        help = 'Disable graph results',
+                        help = 'Disable result visualization',
                         action='store_true',
                         default=False)
     args = parser.parse_args()
--- centerlinexs/crosssections.py
+++ centerlinexs/crosssections.py
@@ -1,263 +1,220 @@
 #!/usr/bin/env python
-import ogr
 from shapely.geometry import *
-import json
-import os
-import math
+import argparse
+import os, sys
 import numpy as np
+from shapes import *
+from plotting import Plotter
+from logger import Logger
 
-# These are for graphic only
-import matplotlib.pyplot as plt
-from matplotlib.collections import PatchCollection
-from descartes import PolygonPatch
+def crosssections(args):
 
+    # --------------------------------------------------------
+    # Load the Shapefiles we need
+    # --------------------------------------------------------
+    log.info("Opening Shapefiles...")
+    rivershp = Shapefile(args.river.name)
+    centerline = Shapefile(args.centerline.name)
+    islandsshp = Shapefile(args.islands.name)
 
-class River:
+    # Pull the geometry objects out and disregard the fields
+    polyRiverShape = rivershp.featuresToShapely()[0]['geometry']
+    centerlines = centerline.featuresToShapely()
 
-    def __init__(self, sRiverShape, sIslands, sCenterLine, sXS):
+    # Load in the island shapes then filter them to qualifying only
+    islList = islandsshp.featuresToShapely()
+    multipolIslands = MultiPolygon([isl['geometry'] for isl in islList if isl['fields']['Qualifying'] == 1])
 
-        # --------------------------------------------------------
-        # Load the Shapefiles we need
-        # --------------------------------------------------------
+    # Make a new rivershape using the exterior and only qualifying islands from that shapefile
+    log.info("Combining exterior and qualifying islands...")
+    rivershape = Polygon(polyRiverShape.exterior).difference(multipolIslands)
 
-        # TODO: This is begging for a little abstraction
-        driver = ogr.GetDriverByName("ESRI Shapefile")
-        dataSource = driver.Open(sRiverShape, 0)
-        riverjson = json.loads(dataSource.GetLayer().GetFeature(0).ExportToJson())['geometry']
-        spatialRef = dataSource.GetLayer().GetSpatialRef()
-        oldrivershape = MultiPolygon([shape(riverjson)])
+    # --------------------------------------------------------
+    # Traverse the line(s)
+    # --------------------------------------------------------
+    log.info("Starting Centerline Traversal...")
+    # Longest possible line we use to extend our cross sections
+    diag = getDiag(rivershape)
+    allxslines = []
+    throwaway = []
 
-        # We're assuming here that the centerline only has one line segment
-        dataSource = driver.Open(sCenterLine, 0)
+    for line in centerlines:
+        linexs = []
+        linegeo = line['geometry']
 
-        centerlines = []
-        for feature in dataSource.GetLayer():
-            cljson = json.loads(feature.ExportToJson())['geometry']
-            centerlines.append({
-                "main": feature.GetField("main"),
-                "line": LineString(shape(cljson))
-            })
+        # Get 50cm spaced points
+        points = [linegeo.interpolate(currDist) for currDist in np.arange(0, linegeo.length, 0.5)]
 
-        # Load in the island shapes
-        dataSource = driver.Open(sIslands, 0)
-        islands = []
+        # TODO: This offset method for slope is a little problematic. Would be nice to find a better slope method using shapely
+        _offsetpts = [linegeo.interpolate(currDist+0.001) for currDist in np.arange(0, linegeo.length, 0.5)]
+        slopes = [((points[idx].coords[0][1] - _offsetpts[idx].coords[0][1]) /
+                   (points[idx].coords[0][0] - _offsetpts[idx].coords[0][0])) for idx, pt in enumerate(_offsetpts)]
 
-        for isl in dataSource.GetLayer():
-            if isl.GetField("Qualifying") == 1:
-                islandjson = json.loads(isl.ExportToJson())['geometry']
-                islands.append(Polygon(shape(islandjson)))
+        # Now create the cross sections with length = 2 * diag
+        for ptidx, pt in enumerate(points):
+            slope = slopes[ptidx]
 
-        # --------------------------------------------------------
-        # Make a new rivershape using the exterior and only
-        # qualifying islands from that shapefile
-        # --------------------------------------------------------
+            # Nothing to see here. Just linear algebra
+            # Make sure to handle the infinite slope case
+            if slope == 0:
+                m = 1
+                k = diag
+            else:
+                # Negative reciprocal is the perpendicular slope
+                m = np.reciprocal(slopes[ptidx]) * -1
+                k = diag / math.sqrt(1 + math.pow(m, 2))
 
-        rivershape = Polygon(oldrivershape[0].exterior).difference(MultiPolygon(islands))
+            # Shoot lines out in both directions using +/- k
+            xsLong = LineString([(pt.coords[0][0] - k, pt.coords[0][1] - k * m),
+                                      (pt.coords[0][0] + k, pt.coords[0][1] + k * m)])
 
-        # --------------------------------------------------------
-        # Traverse the line(s)
-        # --------------------------------------------------------
+            if math.isnan(xsLong.coords[0][1]):
+                print list(xsLong.coords)
 
-        # Longest possible line we use to extend our cross sections
-        diag = getDiag(rivershape)
-        allxslines = []
-        throwaway = []
+            # Make a shape that is just the exterior shape and the qualifying islands
 
-        for line in centerlines:
-            linexs = []
-            # Get 50cm spaced points
-            points = [line["line"].interpolate(currDist) for currDist in np.arange(0, line["line"].length, 0.5)]
+            intersections = rivershape.intersection(xsLong)
+            inlist = []
 
-            # TODO: This offset method for slope is a little problematic. Would be nice to find a better slope method using shapely
-            _offsetpts = [line["line"].interpolate(currDist+0.001) for currDist in np.arange(0, line["line"].length, 0.5)]
-            slopes = [((points[idx].coords[0][1] - _offsetpts[idx].coords[0][1]) /
-                       (points[idx].coords[0][0] - _offsetpts[idx].coords[0][0])) for idx, pt in enumerate(_offsetpts)]
+            # Intersections returns a number of object types. We need to deal with all the valid ones
+            if not intersections.is_empty:
+                if intersections.type == "LineString":
+                    inlist = [intersections]
+                elif intersections.type == "MultiLineString":
+                    inlist = list(intersections)
 
-            # Now create the cross sections with length = 2 * diag
-            for ptidx, pt in enumerate(points):
-                slope = slopes[ptidx]
-
-                # Nothing to see here. Just linear algebra
-                # Make sure to handle the infinite slope case
-                if slope == 0:
-                    m = 1
-                    k = diag
-                else:
-                    # Negative reciprocal is the perpendicular slope
-                    m = np.reciprocal(slopes[ptidx]) * -1
-                    k = diag / math.sqrt(1 + math.pow(m, 2))
-
-                # Shoot lines out in both directions using +/- k
-                xsLong = LineString([(pt.coords[0][0] - k, pt.coords[0][1] - k * m),
-                                          (pt.coords[0][0] + k, pt.coords[0][1] + k * m)])
-
-                if math.isnan(xsLong.coords[0][1]):
-                    print list(xsLong.coords)
-
-                # Make a shape that is just the exterior shape and the qualifying islands
-
-                intersections = rivershape.intersection(xsLong)
-                inlist = []
-
-                # Intersections returns a number of object types. We need to deal with all the valid ones
-                if not intersections.is_empty:
-                    if intersections.type == "LineString":
-                        inlist = [intersections]
-                    elif intersections.type == "MultiLineString":
-                        inlist = list(intersections)
-
-                    for xs in inlist:
-                        keep = True
-                        # Add only lines that contain the centerpoint
-                        if xs.interpolate(xs.project(pt)).distance(pt) > 0.01:
+                for xs in inlist:
+                    keep = True
+                    # Add only lines that contain the centerpoint
+                    if xs.interpolate(xs.project(pt)).distance(pt) > 0.01:
+                        keep = False
+                    # If this is not the main channel and our cross section touches the exterior wall in
+                    # more than one place then lose it
+                    if line['fields']['main'] == "no":
+                        dista = Point(xs.coords[0]).distance(rivershape.exterior)
+                        distb = Point(xs.coords[1]).distance(rivershape.exterior)
+                        if dista < 0.001 and distb < 0.001:
                             keep = False
-                        # If this is not the main channel and our cross section touches the exterior wall in
-                        # more than one place then lose it
-                        if line['main'] == "no":
-                            dista = Point(xs.coords[0]).distance(rivershape.exterior)
-                            distb = Point(xs.coords[1]).distance(rivershape.exterior)
-                            if dista < 0.001 and distb < 0.001:
-                                keep = False
-                        if keep:
-                            linexs.append(xs)
-                        else:
-                            throwaway.append(xs)
+                    if keep:
+                        linexs.append(xs)
+                    else:
+                        throwaway.append(xs)
 
-            allxslines.append(linexs)
+        allxslines.append(linexs)
 
-        # --------------------------------------------------------
-        # Valid vs invalid?
-        # --------------------------------------------------------
-        valid = []
-        invalid = []
-        for xsgroup in allxslines:
-            groupvalid = []
-            groupinvalid = []
-            lengths = [xs.length for xs in xsgroup]
-            stdev = np.std(lengths)
-            mean = np.mean(lengths)
+    # --------------------------------------------------------
+    # Valid/invalid line testing
+    # --------------------------------------------------------
+    log.info("Testin XSs for Validity...")
+    valid = []
+    invalid = []
+    for xsgroup in allxslines:
+        groupvalid = []
+        groupinvalid = []
+        lengths = [xs.length for xs in xsgroup]
+        stdev = np.std(lengths)
+        mean = np.mean(lengths)
 
-            # Test each cross section for validity.
-            # TODO: Right now it's just stddev test. There should probably be others
-            for xs in xsgroup:
-                if xs.length > (mean + 4 * stdev):
-                    groupvalid.append(xs)
-                else:
-                    groupinvalid.append(xs)
-            valid.append(groupvalid)
-            valid.append(groupinvalid)
+        # Test each cross section for validity.
+        # TODO: Right now it's just stddev test. There should probably be others
+        for xs in xsgroup:
+            if xs.length > (mean + 4 * stdev):
+                groupvalid.append(xs)
+            else:
+                groupinvalid.append(xs)
+        valid.append(groupvalid)
+        valid.append(groupinvalid)
 
-        # --------------------------------------------------------
-        # Write the output Shapefile
-        # --------------------------------------------------------
-        schema = {'geometry': 'MultiLineString', 'properties': {'name': 'str'}}
+    # --------------------------------------------------------
+    # Write the output Shapefile
+    # --------------------------------------------------------
+    # TODO: I'd love to abstract all this away but it's a pain to do this in a generic way
+    log.info("Writing Shapefiles...")
+    outShape = Shapefile()
+    outShape.create(args.crosssections, rivershp.spatialRef, geoType=ogr.wkbMultiLineString)
 
-        if os.path.exists(sXS):
-            driver.DeleteDataSource(sXS)
+    # quick and dirty write all xs to shapefile
+    exportlines = []
+    for g in allxslines:
+        for xs in g:
+            exportlines.append(xs)
 
-        outDataSource = driver.CreateDataSource(sXS)
-        outLayer = outDataSource.CreateLayer(sXS, spatialRef, geom_type=ogr.wkbMultiLineString)
+    ogrmultiline = ogr.CreateGeometryFromJson(json.dumps(mapping(MultiLineString(exportlines))))
 
-        # quick and dirty write all xs to shapefile
-        exportlines = []
-        for g in allxslines:
-            for xs in g:
-                exportlines.append(xs)
+    idField = ogr.FieldDefn('name', ogr.OFTString)
+    outShape.layer.CreateField(idField)
 
-        ogrmultiline = ogr.CreateGeometryFromJson(json.dumps(mapping(MultiLineString(exportlines))))
+    featureDefn = outShape.layer.GetLayerDefn()
+    outFeature = ogr.Feature(featureDefn)
+    outFeature.SetGeometry(ogrmultiline)
+    outFeature.SetField('name', ogr.OFTString)
+    outShape.layer.CreateFeature(outFeature)
 
-        idField = ogr.FieldDefn('name', ogr.OFTString)
-        outLayer.CreateField(idField)
-
-        featureDefn = outLayer.GetLayerDefn()
-        outFeature = ogr.Feature(featureDefn)
-        outFeature.SetGeometry(ogrmultiline)
-        outFeature.SetField('name', ogr.OFTString)
-        outLayer.CreateFeature(outFeature)
-
-        # --------------------------------------------------------
-        # Do a little show and tell with plotting and whatnot
-        # --------------------------------------------------------
-        fig = plt.figure(1, figsize=(10, 10))
-        ax = fig.gca()
+    # --------------------------------------------------------
+    # Do a little show and tell with plotting and whatnot
+    # --------------------------------------------------------
+    if not args.noviz:
+        log.info("Plotting Results...")
+        plt = Plotter()
 
         # The shape of the river is grey (this is the one with only qualifying islands
-        plotShape(ax, rivershape, '#AAAAAA', 1, 5)
+        plt.plotShape(rivershape, '#AAAAAA', 1, 5)
 
         # Centerline is black
         for c in centerlines:
-            plotShape(ax, c['line'], '#000000', 0.5, 20)
+            plt.plotShape(c['geometry'], '#000000', 0.5, 20)
 
-        # Throwaway lines (the ones that are too whack to even test for validity) are bright red
-        # plotShape(ax, MultiLineString(throwaway), '#FF0000', 0.5, 20)
+        # Throwaway lines (the ones that are too whack to even test for validity) are faded red
+        plt.plotShape(MultiLineString(throwaway), '#FF0000', 0.1, 20)
 
         # The valid crosssections are blue
         for g in valid:
-            plotShape(ax, MultiLineString(g), '#0000FF', 0.7, 25)
+            plt.plotShape(MultiLineString(g), '#0000FF', 0.7, 25)
         # Invalid crosssections are orange
-        # for g in invalid:
-        #     plotShape(ax, MultiLineString(g), '#00FF00', 0.7, 20)
+        for g in invalid:
+            plt.plotShape(MultiLineString(g), '#00FF00', 0.7, 20)
 
-        plt.autoscale(enable=True)
-        plt.show()
+        plt.showPlot(rivershape.bounds)
 
-
-def getDiag(rect):
-    """
-    return the biggest possible distance inside a rectangle (the diagonal)
-    :param rect: rectangle polygon
-    :return:
-    """
-    return math.sqrt(
-        math.pow((rect.bounds[3] - rect.bounds[1]), 2) +
-        math.pow((rect.bounds[2] - rect.bounds[0]), 2))
-
-def plotShape(ax, mp, color, alpha, zord):
-    """
-    Nothing fancy here. Just a plotting function to help us visualize
-    :param ax:
-    :param mp:
-    :param color:
-    :param alpha:
-    :param zord:
-    :return:
-    """
-    # We're using Descartes here for polygonpathc
-    if mp.type == 'Polygon':
-        ax.add_patch(PolygonPatch(mp, fc=color, ec='#000000', lw=0.2, alpha=alpha, zorder=zord))
-
-    elif mp.type == 'LineString':
-        x, y = mp.xy
-        ax.plot(x, y, color=color, alpha=alpha, linewidth=1, solid_capstyle='round', zorder=zord)
-
-    elif mp.type == 'MultiPoint':
-        for idx, p in enumerate(mp):
-            x, y = p.xy
-            ax.plot(x, y, color=color, alpha=alpha, markersize=2, marker="o", zorder=zord)
-
-    elif mp.type == 'MultiLineString':
-        for idx, p in enumerate(mp):
-            x, y = p.xy
-            ax.plot(x, y, color=color, alpha=alpha, linewidth=1, solid_capstyle='round', zorder=zord)
-
-    elif mp.type == 'MultiPolygon':
-        patches = []
-        for idx, p in enumerate(mp):
-            patches.append(PolygonPatch(p, fc=color, ec='#000000', lw=0.2, alpha=alpha, zorder=zord))
-        ax.add_collection(PatchCollection(patches, match_original=True))
-
-
-def main():
-    '''
-
-    :return:
-    '''
-
-    # We're just iterating over a folder. Change this to something else if you want
-    theRiver = River("../thiessen/sample/WettedExtent.shp", "../thiessen/sample/Islands.shp", "../thiessen/output/centerline.shp", "output/crosssection.shp")
-
-    print "done"
 
 if __name__ == "__main__":
-    main()
+
+    log = Logger("Initializing")
+
+    # parse command line options
+    parser = argparse.ArgumentParser()
+    parser.add_argument('river',
+                        help='Path to the river shape file. Donuts will be ignored.',
+                        type=argparse.FileType('r'))
+    parser.add_argument('islands',
+                        help='Path to the islands shapefile.',
+                        type=argparse.FileType('r'))
+    parser.add_argument('centerline',
+                        help='Path to the centerline shapefile',
+                        type=argparse.FileType('r'))
+    parser.add_argument('crosssections',
+                        help='Path to the desired output crosssections')
+    parser.add_argument('--noviz',
+                        help = 'Disable result visualization',
+                        action='store_true',
+                        default=False)
+    args = parser.parse_args()
+
+    if not args.river or not args.centerline or not args.islands or not args.crosssections:
+        print "ERROR: Missing arguments"
+        parser.print_help()
+        exit(0)
+
+    log = Logger("Program")
+
+    try:
+        crosssections(args)
+    except AssertionError as e:
+        log.error("Assertion Error", e)
+        sys.exit(0)
+    except Exception as e:
+        log.error('Unexpected error: {0}'.format(sys.exc_info()[0]), e)
+        raise
+        sys.exit(0)
+
 
