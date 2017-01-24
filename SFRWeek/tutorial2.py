d8b16b83d3b0ba7792cb514ba214a8a13c5b05ec
Matthew Reimer <matt.reimer@gmail.com>
2017-01-24 15:52:37 -0800

    spiffing up the tutorial

--- SFRWeek/tutorial2.py
+++ SFRWeek/tutorial2.py
@@ -2,11 +2,14 @@ import ogr
 import json
 from shapely.geometry import *
 import matplotlib.pyplot as plt
+import numpy as np
+
 """
 
     Tutorial 2: We're going to open a shape file then close it.
 
 """
+
 sFilename = "inputs/Thalweg.shp"
 
 driver = ogr.GetDriverByName("ESRI Shapefile")
@@ -32,10 +35,17 @@ for feat in rawfeatures:
 thalweg = features[0]['geometry']
 
 
+thalweg2arr = np.array(thalweg.coords)
+thalweg2arr[:,1] += 10
+newthalweg = LineString(thalweg2arr)
+
+
 # Some Plotting
 fig = plt.figure(1, figsize=(10, 10))
 ax = fig.gca()
-ax.plot(*thalweg.xy, color='#FF0000', alpha=0.5, markersize=5, marker="o", zorder=10, label="Some Label")
+ax.plot(*thalweg.xy, color='#FF0000', alpha=1, markersize=5, marker="o", zorder=10, label="Old Thalweg")
+ax.plot(*newthalweg.xy, color='#00FF00', alpha=1, markersize=10, marker="o", zorder=10, label="New Thalweg")
+
 plt.autoscale(enable=False)
 plt.legend(loc='best')
 plt.show()
